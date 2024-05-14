d14b5862e49caf6d69f1eecac2747cdafda0c8fe
MaleeshaAluwihare <159033744+MaleeshaAluwihare@users.noreply.github.com>
2024-05-14 23:34:41 +0530

    Add Navigation on task page

--- app/src/main/java/com/example/trekgo/TaskPageActivity.kt
+++ app/src/main/java/com/example/trekgo/TaskPageActivity.kt
@@ -3,6 +3,7 @@ package com.example.trekgo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.trekgo.databinding.ActivityTaskPageBinding
 
@@ -27,6 +28,24 @@ class TaskPageActivity : AppCompatActivity() {
             val intent = Intent(this,AddTaskActivity::class.java)
             startActivity(intent)
         }
+
+        val back: View = findViewById(R.id.imageView14)
+        val profile: View = findViewById(R.id.imageView8)
+        val homePage: View = findViewById(R.id.imageView3)
+
+        back.setOnClickListener{
+            val intent = Intent(this, home::class.java);
+            startActivity(intent);
+        }
+        profile.setOnClickListener{
+            val intent = Intent(this, settings_page::class.java);
+            startActivity(intent);
+        }
+        homePage.setOnClickListener{
+            val intent = Intent(this, home::class.java);
+            startActivity(intent);
+        }
+
     }
 
     override fun onResume() {
